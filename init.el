4128c37c5f76a9e86479bd990b3f83ec31fe6def
Ian Kerins <ian@DebbieDowner.videogam.es>
2017-10-22 18:51:48 -0400

    Clojure + cider

--- init.el
+++ init.el
@@ -131,7 +131,19 @@
   (setq web-mode-script-padding 2)
   )
 (use-package clojure-mode
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'clj-refactor-mode)
+  (add-hook 'clojure-mode-hook #'aggressive-indent-mode))
+(use-package aggressive-indent
   :ensure t)
+(use-package clj-refactor
+  :ensure t)
+(use-package cider
+  :ensure t
+  :init
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode))
 (use-package paredit
   :ensure t
   :init)
@@ -198,11 +210,10 @@
   :init
   (setq ivy-mode 1)
   (setq ivy-use-virtual-buffers t)
-  )
+  (setq magit-completing-read-function 'ivy-completing-read))
 (use-package swiper
   :ensure t
-  :bind ("C-s" . swiper)
-)
+  :bind ("C-s" . swiper))
 (use-package counsel
   :ensure t
   :bind (("M-x" . counsel-M-x)
